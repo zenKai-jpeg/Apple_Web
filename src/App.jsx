a661e9e174b921b7322587ddbe4bb777e08567b5
zenKai-jpeg <khairul.syahmi@outlook.com>
2024-09-24 15:23:04 +0800

    First Upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
+import Model from "./components/Model-backup"
+
+const App = () => {
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  )
+}
+
+export default App
