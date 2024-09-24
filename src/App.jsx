31b566121cf3110021295ef42f28dc7018c5b3ee
zenKai-jpeg <khairul.syahmi@outlook.com>
2024-09-24 19:10:57 +0800

    remove clash, wrong import address.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
-import Model from "./components/Model-backup"
+import Model from "./components/Model"
 
 const App = () => {
   return (
